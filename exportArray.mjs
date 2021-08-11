42d5c88e279758ae216fdbbe21d1931e4015b374
sandeep210392 <nagasandeep.kudaravalli@gmail.com>
2021-08-11 14:33:22 +0000

    import and export

---
+++ exportArray.mjs
@@ -0,0 +1,13 @@
+let a = "countries";
+let b = 190;
+let c = "continents";
+let d = 7;
+let e = false;
+let f = 6.2;
+
+export default a;
+export default b;
+export default c;
+export default d;
+export default e;
+export default f;
